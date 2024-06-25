f7106dd48aa1536de43305cb89d3fee16adb1281
Bhangdongho <bhang97@naver.com>
2024-06-26 07:58:29 +0900

    데이터 및 타입 생성

--- src/store/slices/boardsSlice.ts
+++ src/store/slices/boardsSlice.ts
@@ -1,8 +1,51 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { IBoard } from "../../types";
 
-const initialState = {
+type TBoardsState = {
+  modalActive: boolean;
+  boardArray: IBoard[];
+};
+
+const initialState: TBoardsState = {
   modalActive: false,
-  boardArray: [],
+  boardArray: [
+    {
+      boardId: "board-0",
+      boardName: "첫번째 게시물",
+      lists: [
+        {
+          listId: "list-0",
+          listName: "List 1",
+          tasks: [
+            {
+              taskId: "task-0",
+              taskName: "Task 1",
+              taskDescription: "Description",
+              taskOwner: "Bhang",
+            },
+            {
+              taskId: "task-1",
+              taskName: "Task 2",
+              taskDescription: "Description",
+              taskOwner: "Kim",
+            },
+          ],
+        },
+        {
+          listId: "list-1",
+          listName: "List 2",
+          tasks: [
+            {
+              taskId: "task-3",
+              taskName: "Task 3",
+              taskDescription: "Description",
+              taskOwner: "Bhang",
+            },
+          ],
+        },
+      ],
+    },
+  ],
 };
 
 const boardsSlice = createSlice({
