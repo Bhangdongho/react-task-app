3300e5fdff0f8165e73c88cb2c2c0528bf7520ef
Bhangdongho <bhang97@naver.com>
2024-06-29 10:34:09 +0900

    12주차 4DAY 과제

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAKp380Y_qYeWJqht5F4F6vRoqizWehQPQ",
+  authDomain: "react-test-app-2-ad06d.firebaseapp.com",
+  projectId: "react-test-app-2-ad06d",
+  storageBucket: "react-test-app-2-ad06d.appspot.com",
+  messagingSenderId: "891255789350",
+  appId: "1:891255789350:web:0c19da46f2d873427b93e7",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
